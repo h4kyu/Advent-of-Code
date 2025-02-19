936b3fdd492b8107d0891b7dcb1ed3e633b6b254
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-02-19 16:09:11 -0500

    part 1 complete

--- 2024/2/main.cpp
+++ 2024/2/main.cpp
@@ -3,36 +3,63 @@
 #include <string>
 #include <sstream>
 
-// read line by line with getLine
 
-    // if diff == 0, unsafe
-    // isIncreasing = diff > 0
+bool is_safe(std::vector<int>& levels) {
+    int prevLevel{levels[0]};
+    int currLevel{levels[1]};
 
-    // if isIncreasing,
+    if (prevLevel == currLevel || abs(currLevel - prevLevel) > 3) return false;
 
-bool is_safe(std::istringstream stream) {
-    int prevLevel, currLevel;
-    stream >> prevLevel >> currLevel;
-    if (prevLevel == currLevel) return false;
+    bool isIncreasing{currLevel - prevLevel > 0};
 
-    bool isIncreasing{prevLevel - currLevel > 0};
-
-    while () { // walk through stream
-
+    if (isIncreasing) {
+        for (int i{2}; i < levels.size(); ++i) {
+            prevLevel = currLevel;
+            currLevel = levels[i];
+            if (abs(currLevel - prevLevel) > 3 || currLevel <= prevLevel) return false;
+        }
+    } else {
+        for (int i{2}; i < levels.size(); ++i) {
+            prevLevel = currLevel;
+            currLevel = levels[i];
+            if (abs(prevLevel - currLevel) > 3 || currLevel >= prevLevel) return false;
+        }
     }
+
+    return true;
+}
+
+
+int part_1(std::string input) {
+    std::istringstream inputStream(input);
+    int safeCount{0};
+    std::string line;
+
+    while (std::getline(inputStream, line)) {
+        std::istringstream stream(line);
+        int level{};
+        std::vector<int> levels{};
+
+        while (stream >> level) {
+            levels.push_back(level);
+        }
+
+        if (is_safe(levels)) ++safeCount;
+    }
+    return safeCount;
 }
 
 int main() {
     // read file line by line
     std::ifstream file("input.txt");
+    // std::ifstream testFile("test.txt");
     if (!file) { // test if file is opened
+        std::cout << "failed" << std::endl;
         return 1;
     }
-    std::string line;
-    while (std::getline(file, line)) {
-        std::istringstream stream(line);
-
-        while ()
-    }
-
+    std::string input((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    // std::string testInput((std::istreambuf_iterator<char>(testFile)), std::istreambuf_iterator<char>());
+    // part 1
+    std::cout << part_1(input) << std::endl;
+    // std::cout << part_1(testInput) << std::endl;
 }
