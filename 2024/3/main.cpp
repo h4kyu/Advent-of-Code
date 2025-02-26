4ab8ddf62db2707ec4e9b1bdc52a8e5e6fcd9d6b
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-02-26 17:11:17 -0500

    wip pseudo code

---
+++ 2024/3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* find all instances of 'mul('
+ * for each instance, check if valid:
+ *  check if ')' exists within 8 characters inclusive
+ *      yes -->
+ *      check for comma:
+ *
+ *
+*/
+
+int main() {
+    return 0;
+}
