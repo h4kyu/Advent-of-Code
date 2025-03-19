bece57d086f4c671aeb0e1c59740b60d0059e5ee
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-03-19 12:05:00 -0400

    fixed part 1 for test input

--- 2024/3/main.cpp
+++ 2024/3/main.cpp
@@ -13,23 +13,23 @@ int part_1(std::string& input) {
     int mulSum{0};
     const std::string delimiter{"mul("};
 
-    size_t searchStart{0};
-    size_t searchEnd{input.find(delimiter)};
+    // size_t searchStart{0};
+    size_t searchStart{input.find(delimiter)};
 
-    while (searchEnd != std::string::npos) {
+    while (searchStart != std::string::npos) {
 
         // check if ')' exists within 8 characters
-        std::string toSearch{input.substr(searchEnd + 4, searchEnd + 12)};
-        size_t pos{toSearch.find(')')};
+        std::string toSearch{input.substr(searchStart + delimiter.size(), 8)}; // maximum of 8 characters between '(' and ')' inclusive
+        size_t pos{toSearch.find(')')}; // zero based index
 
         if (pos != std::string::npos) {
             // search for comma until ')' pos
-            toSearch = toSearch.substr(0, pos + 1);
+            toSearch = toSearch.substr(0, pos); // substr of chars btw '(' and ')'
 
             if (toSearch.find(',') != std::string::npos) {
                 // check if only numbers exist between comma position and '(' and ')'
                 std::string leftOfComma{toSearch.substr(0, toSearch.find(','))};
-                std::string rightOfComma{toSearch.substr(toSearch.find(','))};
+                std::string rightOfComma{toSearch.substr(toSearch.find(',') + 1)};
 
                 if (!leftOfComma.empty() && std::all_of(leftOfComma.begin(), leftOfComma.end(), ::isdigit)
                     && !rightOfComma.empty() && std::all_of(rightOfComma.begin(), rightOfComma.end(), ::isdigit)) {
@@ -39,8 +39,8 @@ int part_1(std::string& input) {
         }
 
         // update search area
-        searchStart = searchEnd + 4;
-        searchEnd = input.find(delimiter, searchEnd);
+        // searchStart = searchEnd + 4;
+        searchStart = input.find(delimiter, searchStart + delimiter.size());
 
     }
     return mulSum;
