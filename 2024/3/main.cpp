44afc6faaec62e777e4fa09e8ed7759e47896eda
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-03-23 21:00:50 -0400

    part_2 complete

--- 2024/3/main.cpp
+++ 2024/3/main.cpp
@@ -115,11 +115,11 @@ int part_2(std::string& input) {
 
 int main() {
     // read file into string input
-    // std::ifstream file("input.txt");
-    // std::string input{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
-
-    std::ifstream file("test.txt");
+    std::ifstream file("input.txt");
     std::string input{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+
+    // std::ifstream file("test.txt");
+    // std::string input{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
 
     std::cout << part_1(input) << std::endl;
     std::cout << part_2(input) << std::endl;
