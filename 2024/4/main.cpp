6f956e663cc32f44c1d2e394915e1888dcb26c82
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-03-31 12:26:56 -0400

    wip

--- 2024/4/main.cpp
+++ 2024/4/main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 /* pseudocode part_1
  * read and store input as 2D array (or matrix?).
@@ -23,22 +24,73 @@ std::string read_file(const char* filename) {
 
 using Grid = std::vector<std::vector<char>>;
 
-bool is_xmas(const char* arr) { // assume arr is 4 characters
+int is_xmas(const char* arr) { // assume arr is 4 characters
     if (arr[0] == 'X' && arr[1] == 'M' && arr[2] == 'A' && arr[3] == 'S') {
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
 
-int part_1(std::string input) {
+int countXMAS(Grid& grid, const size_t row, const size_t col) {
     int count{0};
+
+    char topLeftArr[4] = {grid[row][col], grid[row-1][col-1], grid[row-2][col-2], grid[row-3][col-3]};
+    char topArr[4] = {grid[row][col], grid[row-1][col], grid[row-2][col], grid[row-3][col]};
+    char topRightArr[4] = {grid[row][col], grid[row-1][col+1], grid[row-2][col+2], grid[row-3][col+3]};
+    char rightArr[4] = {grid[row][col+1], grid[row][col+2], grid[row][col+3], grid[row][col+4]};
+    char bottomRightArr[4] = {grid[row][col], grid[row+1][col+1], grid[row+2][col+2], grid[row+3][col+3]};
+    char bottomArr[4] = {grid[row][col], grid[row+1][col], grid[row+2][col], grid[row+3][col]};
+    char bottomLeftArr[4] = {grid[row][col], grid[row+1][col-1], grid[row+2][col-2], grid[row+3][col-3]};
+    char leftArr[4] = {grid[row][col], grid[row][col-1], grid[row][col-2], grid[row][col-3]};
+
+    count += is_xmas(topLeftArr) + is_xmas(topArr) + is_xmas(topRightArr) + is_xmas(rightArr) + is_xmas(bottomRightArr)
+    + is_xmas(bottomArr) + is_xmas(bottomLeftArr) + is_xmas(leftArr);
+
+    return count;
+}
+
+int part_1(std::string& input) {
+    int count{0};
+    std::stringstream inputStream(input);
+    std::string line{};
+    Grid unpaddedGrid;
+
+    while (std::getline(inputStream, line)) {
+        std::vector<char> row(line.begin(), line.end());
+        unpaddedGrid.push_back(row);
+    }
+
+    size_t numRows{unpaddedGrid.size()};
+    size_t numCols{unpaddedGrid[0].size()};
+
+    Grid paddedGrid(numRows + 6, std::vector<char>(numCols + 6, '.'));
+
+    // fill paddedGrid with chars from unpaddedGrid
+    for (size_t i{0}; i < numRows; ++i) {
+        for (size_t j{0}; j < numCols; ++j) {
+            paddedGrid[i+3][j+3] = unpaddedGrid[i][j];
+        }
+    }
+
+    // iterate over chars in paddedGrid
+    for (size_t i{3}; i < numRows+3; ++i) {
+        for (size_t j{3}; j < numCols+3; ++j) {
+            char cell{paddedGrid[i][j]};
+            size_t row{i};
+            size_t col{j};
+            if (cell == 'X') {
+                count += countXMAS(paddedGrid, row, col);
+            }
+        }
+    }
 
 
     return count;
 }
 
 int main() {
-    std::string input{read_file("input.txt")};
+    // std::string input{read_file("input.txt")};
+    std::string input{read_file("test.txt")};
 
     std::cout << part_1(input) << std::endl;
 
