f9914258ed85849f98f0cfbf18d5a5ee6bfa8296
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-04-02 21:01:50 -0400

    pseudocode complete, need to learn about lambda functions in cpp

--- 2024/5/main.cpp
+++ 2024/5/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+/* part 1:
+ * separate rules and updates
+ * split rules by '|' delimiter and store as std::set
+ * split updates by ',' delimiter and store as std::vector
+ * duplicate update and use std::sort to apply all rules to each update, using lambda func as comparator rule for sort
+ * compare newly sorted update with original
+ */
+
+std::string read_file(const char* filename) {
+    std::ifstream file(filename);
+    std::string input{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+
+    return input;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // std::string input{read_file("input.txt")};
+    std::string input{read_file("test.txt")};
+
     return 0;
 }
