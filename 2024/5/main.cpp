c27c6b92c22274fe21ce9cce0ba0aac62f360298
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-04-04 14:11:02 -0400

    added working split and parsing for rules and updates

--- 2024/5/main.cpp
+++ 2024/5/main.cpp
@@ -17,9 +17,34 @@ std::string read_file(const char* filename) {
     return input;
 }
 
+std::pair<int, int> split_rule(const std::string& rule, const char delimiter) {
+    size_t pos{rule.find(delimiter)};
+
+    if (pos == std::string::npos) return {};
+
+    int firstPage{std::stoi(rule.substr(0, pos))};
+    int secondPage{std::stoi(rule.substr(pos+1))};
+
+    return {firstPage, secondPage};
+}
+
+std::vector<int> split_update(const std::string& update, const char delimiter) {
+    std::vector<int> updateParsed{};
+    std::stringstream updateStream(update);
+    std::string page{};
+
+    while (std::getline(updateStream, page, delimiter)) {
+        updateParsed.push_back(std::stoi(page));
+    }
+
+    return updateParsed;
+}
+
 int main() {
     // std::string input{read_file("input.txt")};
     std::string input{read_file("test.txt")};
+
+    split_rule("47|53", '|');
 
     return 0;
 }
