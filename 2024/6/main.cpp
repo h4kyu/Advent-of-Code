e47fbd6d9882d0a0e50641f930fe4d441bf88b5b
Nahshon Weissberg <nahshonweissberg@gmail.com>
2025-05-07 10:54:46 -0400

    Day 6; wip

--- 2024/6/main.cpp
+++ 2024/6/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <unordered_set>
 
 std::string read_file(const char* filename) {
     std::ifstream file(filename);
@@ -18,6 +20,47 @@ std::string read_file(const char* filename) {
  *    TODO keep above looping logic for part 2, but part 1 assumes guard steps out of bounds
  *
  */
+
+class Map {
+private:
+    std::vector<std::vector<char>> currMap;
+    std::unordered_set<char> directions = {'^', '>', 'v', '<'};
+
+    // get guard location
+    std::pair<int, int> get_guard_location(std::vector<std::vector<char>> map) {
+        // iterate over chars in map
+        for (int row{0}; row < map.size(); ++row) {
+            for (int col{0}; col < map[0].size(); ++col) {
+                char currSpace{map[row][col]};
+                // TODO consider storing position of obstacles in first pass
+                if (directions.count(currSpace)) {
+                    return {row, col};
+                }
+            }
+        }
+        return {0, 0};
+    }
+
+    // get guard direction
+
+    // move guard
+public:
+    // constructor, parse input as a 2D array
+    explicit Map(const std::string& input) {
+        std::stringstream ss(input);
+        std::string line{};
+
+        while (std::getline(ss, line)) {
+            currMap.push_back(std::vector<char>(line.begin(), line.end()));
+        }
+
+
+    }
+
+    // get number of tiles traversed
+
+};
+
 
 int main() {
     // std::string input{read_file("input.txt")};
